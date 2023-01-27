96dd6beba02f91771a4216a8966735a1ae1aebd9
Reed von Redwitz <reed+github@vonredwitz.com>
2023-01-27 22:42:52 +0100

    minimal about page

--- routes/about.tsx
+++ routes/about.tsx
@@ -1,7 +1,7 @@
 export default function AboutPage() {
   return (
     <div>
-      <div>Some info about me.</div>
+      <div>I'm a developer living in Germany, originally from California. Here's my github: https://github.com/deer</div>
     </div>
   );
 }
--- utils/newpost.ts
+++ utils/newpost.ts
@@ -1,33 +1,33 @@
 // this whole thing is hacky and bad and needs to get refactored. i hope no one ever sees this, but in the spirit of transparency, i'll commit it
 
-const dirs: Deno.DirEntry[] = [];
+// const dirs: Deno.DirEntry[] = [];
 
-for await (const dirEntry of Deno.readDir("static/screenshots")) {
-  dirs.push(dirEntry);
-}
+// for await (const dirEntry of Deno.readDir("static/screenshots")) {
+//   dirs.push(dirEntry);
+// }
 
-//this doesn't seem to actually filter anything out
-dirs.filter((x) => x.isFile);
+// //this doesn't seem to actually filter anything out
+// dirs.filter((x) => x.isFile);
 
-dirs.sort((x, y) => {
-  return new Date(y.name) - new Date(x.name);
-});
+// dirs.sort((x, y) => {
+//   return new Date(y.name) - new Date(x.name);
+// });
 
-// dirs.forEach((x) => console.log(x));
+// // dirs.forEach((x) => console.log(x));
 
-//because the filter doesn't work, need to grab the second. plz halp
-const bestDir = dirs[1];
+// //because the filter doesn't work, need to grab the second. plz halp
+// const bestDir = dirs[1];
 
-const nextDate = new Date(bestDir.name);
-nextDate.setDate(nextDate.getDate() + 1);
+// const nextDate = new Date(bestDir.name);
+// nextDate.setDate(nextDate.getDate() + 1);
 
-const nextDateName = nextDate.toISOString().split("T")[0];
+// const nextDateName = nextDate.toISOString().split("T")[0];
 
 const now = new Date();
-// const yearMonthDay = now.toISOString().split("T")[0];
+const yearMonthDay = now.toISOString().split("T")[0];
 
 const title: string = Deno.args[0];
-const postDate: string = nextDateName + " " +
+const postDate: string = yearMonthDay + " " +
   padTo2Digits(now.getHours()) +
   ":" +
   padTo2Digits(now.getMinutes());
@@ -40,8 +40,8 @@ date: ${postDate}
 
 `;
 
-await Deno.mkdir(`static/screenshots/${nextDateName}`);
-await Deno.writeTextFile(`posts/${nextDateName}.md`, newPostTemplate);
+await Deno.mkdir(`static/screenshots/${yearMonthDay}`);
+await Deno.writeTextFile(`posts/${yearMonthDay}.md`, newPostTemplate);
 
 function padTo2Digits(num: number) {
   return String(num).padStart(2, "0");
