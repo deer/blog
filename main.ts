660978ac1daffd6ebca40dd62dcadb322b8d68a1
Reed von Redwitz <reed+github@vonredwitz.com>
2023-07-30 15:48:23 +0200

    refactor to use plugins (#1)

--- main.ts
+++ main.ts
@@ -10,4 +10,24 @@ import manifest from "./fresh.gen.ts";
 import twindPlugin from "$fresh/plugins/twind.ts";
 import twindConfig from "./twind.config.ts";
 
-await start(manifest, { plugins: [twindPlugin(twindConfig)] });
+import {
+  BlogOptions,
+  blogPlugin,
+} from "https://deno.land/x/fresh_blog@0.0.2/mod.ts";
+
+import { ga4Plugin } from "https://deno.land/x/fresh_ga4@0.0.1/mod.ts";
+
+const blogOptions: BlogOptions = {
+  title: "Reed's Blog",
+  rootPath: import.meta.url,
+  navbarItems: {
+    Archive: "/archive",
+    About: "/about",
+    Contact: "/contact",
+    Projects: "/projects",
+  },
+};
+
+await start(manifest, {
+  plugins: [twindPlugin(twindConfig), blogPlugin(blogOptions), ga4Plugin()],
+});
