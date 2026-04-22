8de87f03d019364a2ab2269affdf80d1d55cb39e
Reed von Redwitz <reed+github@vonredwitz.com>
2026-04-22 20:38:12 +0200

    chore: debug route

---
+++ routes/debug-headers.ts
@@ -0,0 +1,11 @@
+import { define } from "../utils.ts";
+
+export const handler = define.handlers({
+  GET(ctx) {
+    const headers: Record<string, string> = {};
+    ctx.req.headers.forEach((value, key) => {
+      headers[key] = value;
+    });
+    return Response.json(headers);
+  },
+});
